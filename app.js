514619378d8ffa678e1a8382deba569306c931d6
RSMarian <marianstefan1499@gmail.com>
2022-07-01 18:10:09 +0300

    added comments

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ let load = 0;
 
 let int = setInterval(blurring, 30);
 
+// 0-100% loading function
 function blurring() {
   load++;
   if (load > 99) {
     clearInterval(int);
   }
   loadText.innerText = `${load}%`;
+  // text&bg blur
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
+// **SET SCALE**
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
